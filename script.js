676af912221e624a1f335db94f9e994120ab6e44
manami1111 <manamiii0319@gmail.com>
2024-07-09 14:25:45 +0900

    for文追加

--- script.js
+++ script.js
@@ -10,32 +10,56 @@ const I = document.querySelector('#I');
 
 let count = 0;
 
-A.addEventListener('click', () => {
-    count++;
-    // 奇数偶数の判定
-    if (count%2 === 1){
-        A.textContent = '〇'
-    } else {
-        A.textContent = '✖'
-    }
-});
+// A.addEventListener('click', () => {
+//     count++;
+//     // 奇数偶数の判定
+//     if (count%2 === 1){
+//         A.textContent = '〇'
+//     } else {
+//         A.textContent = '✖'
+//     }
+// });
 
-B.addEventListener('click', () => {
-    count++;
-    // 奇数偶数の判定
-    if (count%2 === 1){
-        B.textContent = '〇'
-    } else {
-        B.textContent = '✖'
-    }
-});
+// B.addEventListener('click', () => {
+//     count++;
+//     if (count%2 === 1){
+//         B.textContent = '〇'
+//     } else {
+//         B.textContent = '✖'
+//     }
+// });
 
-C.addEventListener('click', () => {
-    count++;
-    // 奇数偶数の判定
-    if (count%2 === 1){
-        C.textContent = '〇'
-    } else {
-        C.textContent = '✖'
-    }
-});
+// C.addEventListener('click', () => {
+//     count++;
+//     if (count%2 === 1){
+//         C.textContent = '〇'
+//     } else {
+//         C.textContent = '✖'
+//     }
+// });
+
+// D.addEventListener('click', () => {
+//     count++;
+//     if (count%2 === 1){
+//         D.textContent = '〇'
+//     } else {
+//         D.textContent = '✖'
+//     }
+// });
+
+// これは配列が入る
+const masu = document.querySelectorAll('.board div');
+console.log(masu);
+
+
+for (let i = 0; i < 9; i++){
+    masu[i].addEventListener('click', () => {
+        count++;
+        if (count % 2 === 1){
+            masu[i].textContent = '〇'
+        } else {
+            masu[i].textContent = '✖'
+        }
+    });
+    
+}
