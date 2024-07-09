e7c5a9ce9b8c44d66c087212191ef43491e7fe67
manami1111 <manamiii0319@gmail.com>
2024-07-09 13:38:19 +0900

    js追加

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const A = document.querySelector('#A');
+const B = document.querySelector('#B');
+const C = document.querySelector('#C');
+const D = document.querySelector('#D');
+const E = document.querySelector('#E');
+const F = document.querySelector('#F');
+const G = document.querySelector('#G');
+const H = document.querySelector('#H');
+const I = document.querySelector('#I');
+
+let count = 0;
 
 A.addEventListener('click', () => {
-    A.textContent = '〇'
-})
+    count++;
+    // 奇数偶数の判定
+    if (count%2 === 1){
+        A.textContent = '〇'
+    } else {
+        A.textContent = '✖'
+    }
+});
